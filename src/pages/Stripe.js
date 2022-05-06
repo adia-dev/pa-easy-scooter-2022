13e86058e72525418408eaf3d9106187535f3124
paschyz <paschy2002@gmail.com>
2022-05-06 11:20:44 +0200

    stripe installation

--- src/pages/Stripe.js
+++ src/pages/Stripe.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import Header from '../components/Header'
 
 const Stripe = () => {
   return (
-    <div>Stripe</div>
+    <div>
+        <Header/>
+    </div>
   )
 }
 
