24dfbfa9a9237859fc6f135df85180ee1c7e2c88
pascal <paschy2002@gmail.com>
2022-05-08 23:17:21 +0200

    added link to check-out

--- src/pages/Stripe.js
+++ src/pages/Stripe.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import Header from '../components/Header'
+import { Link } from 'react-router-dom'
+
+
 
 const Stripe = () => {
   return (
     <div>
         <Header/>
+      <a href='https://buy.stripe.com/test_bIY29y6u5g0GgmcaEE'>Shop</a>
     </div>
   )
 }
