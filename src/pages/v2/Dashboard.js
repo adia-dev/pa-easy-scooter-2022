649ff19a2a27677333760db5db4af74c106ab6bd
Abdoulaye Dia <abdoulaye@jgandco.consulting>
2022-05-31 01:08:21 +0200

    Dashboard v2 front done

--- src/pages/v2/Dashboard.js
+++ src/pages/v2/Dashboard.js
@@ -1,14 +1,19 @@
 import { AiOutlineMessage, AiOutlinePieChart } from "react-icons/ai"
 import { BiFilter, BiSearch, BiStats } from "react-icons/bi"
-import { FaBell } from "react-icons/fa"
-import { GiCalendarHalfYear, GiReturnArrow } from "react-icons/gi"
+import { BsThreeDots } from "react-icons/bs"
+import { FaBell, FaUsers } from "react-icons/fa"
+import { GiCalendarHalfYear, GiFullMotorcycleHelmet, GiReturnArrow } from "react-icons/gi"
+import { GoBroadcast } from "react-icons/go"
 import { IoMdArrowDropdown, IoMdArrowDropright } from "react-icons/io"
+import { MdOutlineElectricScooter } from "react-icons/md"
 import { RiUser6Line } from "react-icons/ri"
 import { SiCircle } from "react-icons/si"
 
 const Dashboard2 = () => {
+
+
     return (
-        <div className="p-5">
+        <div className="bg-gray-50 min-h-screen p-5 w-screen overflow-x-hidden">
 
             <div className="flex items-center ">
                 <div className="font-semibold lowercase">easy scooter</div>
@@ -120,6 +125,205 @@ const Dashboard2 = () => {
                         </div>
                     </div>
                 </div>
+            </div>
+
+            <div className="flex items-center space-x-4 mt-5">
+                <h1 className="font-semibold">Numbers</h1>
+                <div className="flex items-center text-gray-500 text-xs">
+                    <span>Basic preset</span>
+                    <IoMdArrowDropdown />
+                </div>
+            </div>
+
+            <div className="flex items-center w-full space-x-6 mt-3">
+                <div className="bg-white flex-1 aspect-[1/0.5] rounded-xl flex flex-col">
+                    <div className="flex items-center space-x-3 p-5 border-b border-gray-100">
+                        <FaUsers size={32} />
+                        <span>Users</span>
+                    </div>
+                    <div className="flex items-center p-3 flex-1 w-full">
+                        <div className="flex-1 border-r border-gray-100 w-full flex-col items-center justify-center text-center ">
+                            <p className="font-semibold text-3xl mb-2">329</p>
+                            <p className="text-xs text-gray-400">Total Users</p>
+                        </div>
+                        <div className="flex-1 w-full flex-col items-center justify-center text-center ">
+                            <p className="font-semibold text-3xl mb-2">93.5%</p>
+                            <p className="text-xs text-gray-400">Growth</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="bg-white flex-1 aspect-[1/0.5] rounded-xl flex flex-col">
+                    <div className="flex items-center space-x-3 p-5 border-b border-gray-100">
+                        <MdOutlineElectricScooter size={32} />
+                        <span>Scooters</span>
+                    </div>
+                    <div className="flex items-center p-3 flex-1 w-full">
+                        <div className="flex-1 border-r border-gray-100 w-full flex-col items-center justify-center text-center ">
+                            <p className="font-semibold text-3xl mb-2">329</p>
+                            <p className="text-xs text-gray-400">Total Scooters</p>
+                        </div>
+                        <div className="flex-1 border-r border-gray-100 w-full flex-col items-center justify-center text-center ">
+                            <p className="font-semibold text-3xl mb-2">2</p>
+                            <p className="text-xs text-gray-400">In repair</p>
+                        </div>
+                        <div className="flex-1 w-full flex-col items-center justify-center text-center ">
+                            <p className="font-semibold text-3xl mb-2">93.5%</p>
+                            <p className="text-xs text-gray-400">Growth</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="bg-white flex-1 aspect-[1/0.5] rounded-xl flex flex-col">
+                    <div className="flex items-center space-x-3 p-5 border-b border-gray-100">
+                        <GoBroadcast size={32} />
+                        <span>Bookings</span>
+                    </div>
+                    <div className="flex items-center p-3 flex-1 w-full">
+                        <div className="flex-1 border-r border-gray-100 w-full flex-col items-center justify-center text-center ">
+                            <p className="font-semibold text-3xl mb-2">17</p>
+                            <p className="text-xs text-green-500">Live</p>
+                        </div>
+                        <div className="flex-1 w-full flex-col items-center justify-center text-center ">
+                            <p className="font-semibold text-3xl mb-2">93.5%</p>
+                            <p className="text-xs text-gray-400">Growth</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="bg-white flex-1 aspect-[1/0.5] rounded-xl flex flex-col">
+                    <div className="flex items-center space-x-3 p-5 border-b border-gray-100">
+                        <GiFullMotorcycleHelmet size={32} />
+                        <span>Accessories</span>
+                    </div>
+                    <div className="flex items-center p-3 flex-1 w-full">
+                        <div className="flex-1 border-r border-gray-100 w-full flex-col items-center justify-center text-center ">
+                            <p className="font-semibold text-3xl mb-2">329</p>
+                            <p className="text-xs text-gray-400">Sold</p>
+                        </div>
+                        <div className="flex-1 w-full flex-col items-center justify-center text-center ">
+                            <p className="font-semibold text-3xl mb-2">93.5%</p>
+                            <p className="text-xs text-gray-400">Growth</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="flex items-center space-x-4 mt-5">
+                <h1 className="font-semibold">Users</h1>
+                <div className="flex items-center text-gray-500 text-xs">
+                    <span>232</span>
+                    <IoMdArrowDropdown />
+                </div>
+            </div>
+
+
+            <div class="flex flex-col mt-10 overflow-x-auto w-full align-middle overflow-hidden">
+                <table class="overflow-y-scroll  ">
+                    <thead class=" ">
+                        <tr>
+                            <th scope="col" class="p-4">
+                                <div class="flex items-center">
+                                    <input id="checkbox-all" type="checkbox" class="w-4 h-4 bg-gray-100 rounded border-gray-300  focus:ring-2  " />
+                                    <label for="checkbox-all" class="sr-only">checkbox</label>
+                                </div>
+                            </th>
+                            <th scope="col" class="py-3 text-xs font-medium tracking-wider text-left uppercase ">
+                                Full Name
+                            </th>
+                            <th scope="col" class="py-3 text-xs font-medium tracking-wider text-left uppercase ">
+                                Email
+                            </th>
+                            <th scope="col" class="py-3 text-xs font-medium tracking-wider text-left uppercase ">
+                                Role
+                            </th>
+                            <th scope="col" class="py-3 text-xs font-medium tracking-wider text-left uppercase ">
+                                Last Activity
+                            </th>
+                            <th scope="col" class="py-3 text-xs font-medium tracking-wider text-left uppercase ">
+                                Status
+                            </th>
+                            <th scope="col" class="py-3 text-xs font-medium tracking-wider text-left uppercase ">
+
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody class="bg-white cursor-pointer ">
+                        <tr class="hover:bg-gray-100">
+                            <td class="p-4 w-4">
+                                <div class="flex items-center">
+                                    <input id="checkbox-table-1" type="checkbox" class="w-4 h-4 bg-gray-100 rounded border-gray-300  focus:ring-2" />
+                                    <label for="checkbox-table-1" class="sr-only" >checkbox</label>
+                                </div>
+                            </td>
+                            <td class="py-4 text-sm font-medium text-gray-400 whitespace-nowrap ">Abdoulaye Dia</td>
+                            <td class="py-4 text-sm font-medium text-gray-400 whitespace-nowrap ">abdou@easyscooter.com</td>
+                            <td class="py-4 text-sm font-medium text-gray-400 whitespace-nowrap ">Admin</td>
+                            <td class="py-4 text-sm font-medium text-gray-400 whitespace-nowrap ">31/05/2022 08:02</td>
+                            <td class="py-4 text-sm font-medium text-gray-400 whitespace-nowrap "><span className='flex items-center justify-center text-green-800 bg-green-100 w-fit px-2 py-1 text-xs rounded-full'>Completed</span></td>
+                            <td class="py-4 text-sm font-medium text-right whitespace-nowrap">
+                                <div class="text-gray-400 hover:scale-110 transition-all duration-200 ease-in-out">
+                                    <BsThreeDots size={20} />
+                                </div>
+                            </td>
+                        </tr>
+                        <tr class="hover:bg-gray-100 ">
+                            <td class="p-4 w-4">
+                                <div class="flex items-center">
+                                    <input id="checkbox-table-1" type="checkbox" class="w-4 h-4 bg-gray-100 rounded border-gray-300  focus:ring-2" />
+                                    <label for="checkbox-table-1" class="sr-only" >checkbox</label>
+                                </div>
+                            </td>
+                            <td class="py-4 text-sm font-medium text-gray-400 whitespace-nowrap ">(deleted)</td>
+                            <td class="py-4 text-sm font-medium text-gray-400 whitespace-nowrap ">xxx@xxx.xxx</td>
+                            <td class="py-4 text-sm font-medium text-gray-400 whitespace-nowrap ">Customer</td>
+                            <td class="py-4 text-sm font-medium text-gray-400 whitespace-nowrap ">31/05/2022 08:02</td>
+                            <td class="py-4 text-sm font-medium text-gray-400 whitespace-nowrap "><span className='flex items-center justify-center text-red-800 bg-red-100 w-fit px-2 py-1 text-xs rounded-full'>Deleted</span></td>
+                            <td class="py-4 text-sm font-medium text-right whitespace-nowrap">
+                                <div class="text-gray-400 hover:scale-110 transition-all duration-200 ease-in-out">
+                                    <BsThreeDots size={20} />
+                                </div>
+                            </td>
+                        </tr>
+                        <tr class="hover:bg-gray-100 ">
+                            <td class="p-4 w-4">
+                                <div class="flex items-center">
+                                    <input id="checkbox-table-1" type="checkbox" class="w-4 h-4 bg-gray-100 rounded border-gray-300  focus:ring-2" />
+                                    <label for="checkbox-table-1" class="sr-only" >checkbox</label>
+                                </div>
+                            </td>
+                            <td class="py-4 text-sm font-medium text-gray-400 whitespace-nowrap ">Pascal Zhou</td>
+                            <td class="py-4 text-sm font-medium text-gray-400 whitespace-nowrap ">pascal@easyscooter.com</td>
+                            <td class="py-4 text-sm font-medium text-gray-400 whitespace-nowrap ">Partner</td>
+                            <td class="py-4 text-sm font-medium text-gray-400 whitespace-nowrap ">06/06/2022 2:22</td>
+                            <td class="py-4 text-sm font-medium text-gray-400 whitespace-nowrap ">
+                                <span className='flex items-center justify-center text-blue-800 bg-blue-100 w-fit px-2 py-1 text-xs rounded-full'>Pending</span>
+                            </td>
+                            <td class="py-4 text-sm font-medium text-right whitespace-nowrap">
+                                <div class="text-gray-400 hover:scale-110 transition-all duration-200 ease-in-out">
+                                    <BsThreeDots size={20} />
+                                </div>
+                            </td>
+                        </tr>
+                        <tr class="hover:bg-gray-100 ">
+                            <td class="p-4 w-4">
+                                <div class="flex items-center">
+                                    <input id="checkbox-table-1" type="checkbox" class="w-4 h-4 bg-gray-100 rounded border-gray-300  focus:ring-2" />
+                                    <label for="checkbox-table-1" class="sr-only" >checkbox</label>
+                                </div>
+                            </td>
+                            <td class="py-4 text-sm font-medium text-gray-400 whitespace-nowrap line-through">Yann HABIE</td>
+                            <td class="py-4 text-sm font-medium text-gray-400 whitespace-nowrap line-through">yann@easyscooter.com</td>
+                            <td class="py-4 text-sm font-medium text-gray-400 whitespace-nowrap ">Customer</td>
+                            <td class="py-4 text-sm font-medium text-gray-400 whitespace-nowrap ">17/05/2022 18:02</td>
+                            <td class="py-4 text-sm font-medium text-gray-400 whitespace-nowrap ">
+                                <span className='flex items-center justify-center text-orange-800 bg-orange-100 w-fit px-2 py-1 text-xs rounded-full'>Banned</span>
+                            </td>
+                            <td class="py-4 text-sm font-medium text-right whitespace-nowrap">
+                                <div class="text-gray-400 hover:scale-110 transition-all duration-200 ease-in-out">
+                                    <BsThreeDots size={20} />
+                                </div>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
 
         </div>
