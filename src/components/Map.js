85a1413047981eec601377244ae24b1e1d0c36b9
Abdoulaye Dia <abdoulaye@jgandco.consulting>
2022-07-11 23:57:55 +0200

    display the pickup points

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,12 +1,13 @@
 import React, { useMemo, useState } from 'react';
 
 import { Circle, DirectionsRenderer, GoogleMap, Marker, MarkerClusterer } from "@react-google-maps/api";
+import axios from 'axios';
 import { useCallback } from 'react';
 import { closeOptions } from '../data/googleMapsOptions';
 
 // 6 Rue Gerbe
 
-const Map = ({ mapRef, target, isLoaded, centerCoords, customOptions }) => {
+const Map = ({ mapRef, target, isLoaded, centerCoords, customOptions, pickupPoints, setPickupPoints }) => {
 
     const google = window.google;
 
@@ -14,10 +15,28 @@ const Map = ({ mapRef, target, isLoaded, centerCoords, customOptions }) => {
     const options = useMemo(() => customOptions ?? { mapId: "3bb35e645c690444", disableDefaultUI: false, clickableIcons: false }, [])
     const scooters = useMemo(() => generateScooters(target), [target])
     const [directions, setDirections] = useState(null)
+    const [currentCoords, setCurrentCoords] = useState(null)
+
+
 
     const onLoad = useCallback(
         (map) => {
             mapRef.current = map;
+            const fetchPickupPoints = async () => {
+                const data = await axios.get("http://localhost:5500/api/v2/pickups");
+                console.log(data.data);
+
+                if (navigator && navigator.geolocation) {
+                    navigator.geolocation.getCurrentPosition(pos => {
+                        const coords = pos.coords;
+                        console.log(coords)
+                        setCurrentCoords({ lat: coords.latitude, lng: coords.longitude })
+                    });
+                }
+                setPickupPoints(data.data)
+            }
+
+            fetchPickupPoints();
         },
         [],
     )
@@ -42,16 +61,41 @@ const Map = ({ mapRef, target, isLoaded, centerCoords, customOptions }) => {
         )
     }
 
+    const handleClickOnPickupPoint = (coords) => {
+        if (!coords) return;
+
+        console.log(coords);
+        console.log(center);
+
+        const googleMapsDirectionService = new google.maps.DirectionsService();
+        googleMapsDirectionService.route(
+            {
+                // origin: currentCoords ?? center,
+                origin: center,
+                destination: coords,
+                travelMode: google.maps.TravelMode.BICYCLING
+            },
+            (result, status) => {
+                console.log(result);
+                console.log(status);
+                if (status === "OK" && result)
+                    setDirections(result);
+            }
+        )
+    }
+
 
     if (!isLoaded) return <div>Loading...</div>
 
     return (
         <div className='w-full h-full bg-red-200'>
             <GoogleMap onLoad={onLoad} zoom={11} center={center} mapContainerClassName="w-full h-full" options={options}>
+
+                {directions && <DirectionsRenderer directions={directions} />}
+
                 {target &&
                     <>
 
-                        {directions && <DirectionsRenderer directions={directions} />}
 
                         <Marker position={target} />
                         <Circle center={target} radius={15000} options={closeOptions} />
@@ -60,10 +104,25 @@ const Map = ({ mapRef, target, isLoaded, centerCoords, customOptions }) => {
                             enableRetinaIcons={true}
                             gridSize={250}>
                             {(clusterer) =>
-                                scooters.length > 0 && scooters.map((scooter, i) => <Marker onClick={() => handleClickOnMaker(scooter)} key={i} position={scooter} clusterer={clusterer} />)
+                                scooters.length > 0 && scooters.map((scooter, i) => <Marker key={i} onClick={() => handleClickOnMaker(scooter)} position={scooter} clusterer={clusterer} />)
                             }
                         </MarkerClusterer>
+
                     </>}
+                {
+                    pickupPoints.length > 0 &&
+                    <MarkerClusterer
+                        averageCenter={true}
+                        enableRetinaIcons={true}
+                        gridSize={250}>
+                        {(clusterer) => {
+                            console.log("pickup points");
+                            console.log(pickupPoints);
+                            return pickupPoints.length > 0 && pickupPoints.map((point, i) => <Marker key={i} onClick={() => handleClickOnPickupPoint({ lat: point.lat, lng: point.lng })} position={{ lat: point.lat, lng: point.lng }} clusterer={clusterer} />)
+                        }
+                        }
+                    </MarkerClusterer>
+                }
             </GoogleMap>
         </div>
     )
@@ -85,6 +144,5 @@ const generateScooters = (position) => {
             lng: position.lng + Math.random() / directionY,
         });
     }
-    console.log(_scooters)
     return _scooters;
 };
